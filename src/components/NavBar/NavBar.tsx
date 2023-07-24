9f95c92ed05cc23b49443f68c1bd09ff2f97950a
Alexey Rusel <alexeyrusel@gmail.com>
2023-07-24 17:59:31 +0300

    feat: implement change theme feature

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,18 +1,24 @@
-import { FC } from "react";
+import { FC, SyntheticEvent } from "react";
 import { Link } from "react-router-dom";
 
 import logo from "@/assets/logo.svg";
 import { NavList } from "@/components";
 import { AppRoutes } from "@/constants";
-import { useAppDispatch } from "@/hooks";
+import { useAppDispatch, useAppSelector } from "@/hooks";
 import { themeActions } from "@/store/themeSlice";
 
 import styles from "./NavBar.module.scss";
 
 export const NavBar: FC = () => {
     const dispatch = useAppDispatch();
+    const darkTheme = useAppSelector((state) => state.theme.darkTheme);
 
-    const handleClick = () => {
+    const handleClick = (e: SyntheticEvent) => {
+        const target = e.target as HTMLInputElement;
+        if (target.value === darkTheme.toString()) {
+            return;
+        }
+
         dispatch(themeActions.toggleTheme());
     };
 
@@ -22,9 +28,28 @@ export const NavBar: FC = () => {
                 <img src={logo} alt="logo" />
             </Link>
             <NavList />
-            <button type="button" onClick={handleClick}>
-                Change Theme
-            </button>
+            <div
+                className={
+                    darkTheme ? `${styles.radios} ${styles.radios_dark_theme}` : `${styles.radios}`
+                }>
+                <input
+                    type="radio"
+                    id={styles.falseRadio}
+                    className={styles.input}
+                    name="theme"
+                    value="false"
+                    onClick={handleClick}
+                />
+                <input
+                    type="radio"
+                    id={styles.trueRadio}
+                    className={styles.input}
+                    name="theme"
+                    value="true"
+                    defaultChecked={darkTheme}
+                    onClick={handleClick}
+                />
+            </div>
         </nav>
     );
 };
