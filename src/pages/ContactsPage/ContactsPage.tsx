ef828f35a8a7c46f1b4d7c77e0c6a5daa0970a65
Alexey Rusel <alexeyrusel@gmail.com>
2023-07-30 00:41:47 +0300

    feat: add contacts page

--- src/pages/ContactsPage/ContactsPage.tsx
+++ src/pages/ContactsPage/ContactsPage.tsx
@@ -1,11 +1,42 @@
 import { FC } from "react";
 
+import { useAppSelector } from "@/hooks";
+
 import styles from "./ContactsPage.module.scss";
 
 interface ContactsPageProps {}
 
-const ContactsPage: FC<ContactsPageProps> = () => (
-    <div className={styles.ContactsPage}>ContactsPage Component</div>
-);
+const ContactsPage: FC<ContactsPageProps> = () => {
+    const darkTheme = useAppSelector((state) => state.theme.darkTheme);
+
+    const linkStyles = darkTheme ? `${styles.link} ${styles.linkDarkTheme}` : `${styles.link}`;
+
+    return (
+        <div>
+            <div className={styles.row}>
+                <a className={linkStyles} href="tel:+375299999999">
+                    Phone: +375 (29) 999-99-99
+                </a>
+                <ul className={styles.list}>
+                    <li>
+                        <a className={linkStyles} href="https://www.youtube.com/">
+                            YouTube
+                        </a>
+                    </li>
+                    <li>
+                        <a className={linkStyles} href="https://www.facebook.com/">
+                            Facebook
+                        </a>
+                    </li>
+                    <li>
+                        <a className={linkStyles} href="https://www.instagram.com/">
+                            Instagram
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+};
 
 export default ContactsPage;
