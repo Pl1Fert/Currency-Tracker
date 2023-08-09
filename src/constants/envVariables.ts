22b273e8bb65d0b70b2cb6db1914359d24b384d0
Alexey Rusel <alexeyrusel@gmail.com>
2023-08-09 10:39:24 +0300

    feat: change env vars

--- src/constants/envVariables.ts
+++ src/constants/envVariables.ts
@@ -1,17 +1,17 @@
-const { CURRENCY_API_KEY } = process.env;
-const { CURRENCY_API_URL } = process.env;
-const { COIN_API_KEY } = process.env;
-const { COIN_API_URL } = process.env;
-const { MAPBOX_API_KEY } = process.env;
-const { TRIPMAP_API_KEY } = process.env;
-const { TRIPMAP_API_URL } = process.env;
+// const CURRENCY_API_KEY = process.env.CURRENCY_API_KEY;
+// const CURRENCY_API_URL = process.env.CURRENCY_API_URL;
+// const { COIN_API_KEY } = process.env;
+// const { COIN_API_URL } = process.env;
+// const { MAPBOX_API_KEY } = process.env;
+// const { TRIPMAP_API_KEY } = process.env;
+// const { TRIPMAP_API_URL } = process.env;
 
 export const ENV_VARS = {
-    CURRENCY_API_KEY,
-    CURRENCY_API_URL,
-    MAPBOX_API_KEY,
-    TRIPMAP_API_KEY,
-    TRIPMAP_API_URL,
-    COIN_API_KEY,
-    COIN_API_URL,
+    CURRENCY_API_KEY: process.env.CURRENCY_API_KEY,
+    CURRENCY_API_URL: process.env.CURRENCY_API_URL,
+    MAPBOX_API_KEY: process.env.MAPBOX_API_KEY,
+    TRIPMAP_API_KEY: process.env.TRIPMAP_API_KEY,
+    TRIPMAP_API_URL: process.env.TRIPMAP_API_URL,
+    COIN_API_KEY: process.env.COIN_API_KEY,
+    COIN_API_URL: process.env.COIN_API_URL,
 };
